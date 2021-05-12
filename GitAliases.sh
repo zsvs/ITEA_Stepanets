bddf0eef5b4cc28609b4258d1885970e0852efe7
Stepanets Valerii <stepanezc@gmail.com>
2021-05-12 20:32:19 +0300

    Shell script for setting up git aliases

---
+++ GitAliases.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo Setting up aliases
+echo $PATH
+alias gss="git status"
+alias ga="git add"
+alias gb="git branch"
+alias gc="git commit"
+alias gd="git diff"
+alias gco="git checkout"

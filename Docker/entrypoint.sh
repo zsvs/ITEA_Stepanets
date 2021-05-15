8d685182a140c36f65c2540521813c9cbf25b0bf
Stepanets Valerii <stepanezc@gmail.com>
2021-05-15 13:32:42 +0300

    Some settings for nginx start as daemon and repalce placeholder in index.html

---
+++ Docker/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir 1
+
+if [ $? == 0 ]
+then
+  echo "Starting nginx"
+  search_str="%NM%"
+  file="/usr/share/nginx/html/index.html"
+  sed -i "s/$search_str/$MY_NAME/gi" $file
+  nginx -g "daemon off;"
+fi

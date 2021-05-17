bf09442aedfb29dc645c9132474c71c505eb20fd
zsvs <62026632+zsvs@users.noreply.github.com>
2021-05-17 16:18:57 +0300

    Add files via upload

--- Docker/PHP-FPM/entrypoint_php.sh
+++ Docker/PHP-FPM/entrypoint_php.sh
@@ -1,13 +1,30 @@
-#!/bin/bash
-
-echo "list /var/www/HomeWorkPHP.com=========="
-ls -lai /var/www/HomeWorkPHP.com
-
+#!/usr/bin/bash
+  #search_str="/run/php/php7.4-fpm.sock"
+  #replace_str="127.0.0.1:80"
+  #file="/etc/php/7.4/fpm/pool.d/www.conf"
+  #sed -i "s/$search_str/$replace_str/" $file
+mkdir 1
 if [ $? == 0 ]
 then
-  chown -R $USER:$USER /var/www/HomeWorkPHP.com
-
   nginx -t
+
+
+  echo "+++++++++++++++++CHEKING nginx+++++++++++++++++++++++++"
+  ls -la /etc/nginx/conf.d/
+  echo "++++++++++++++++++++++++++++++++++++++++++"
+
+  cat /etc/nginx/sites-enabled/default
+
+  echo "++++++++++++++++++++++++++++++++++++++++++"
+
+  cat /etc/php/7.4/fpm/pool.d/www.conf | grep listen
+
+  echo "+++++++++++++++CHEKING PHP-FPM STATUS++++++++++++++++++++"
+
+  service php7.4-fpm start
+  service php7.4-fpm status
+
+  echo "++++++++++++++++++++++++++++++++++++++++++"
 
   echo "Starting nginx"
   nginx -g "daemon off;"

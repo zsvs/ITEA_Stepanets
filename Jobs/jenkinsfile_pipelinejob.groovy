5b2a8f0e1e46269428b46f09abf6642f912ac104
ZSVS <stepanezc@gmail.com>
2021-06-22 15:14:02 +0300

    Added Jobs

---
+++ Jobs/jenkinsfile_pipelinejob.groovy
@@ -0,0 +1,26 @@
+pipelineJob("docker-nginx-test") {
+    	logRotator {
+            numToKeep(10)
+    	}
+
+    parameters {
+      stringParam('MY_NAME', 'default', 'Set environment variable')
+	booleanParam('DOCKER_PUSH', true, 'Push to docker hub')
+    }
+
+    definition {
+        cpsScm {
+   	      scm {
+                  git {
+                        remote {
+                              github("zsvs/ITEA_Stepanets", 'https')
+                              branch("Jenkins")
+                              }
+                        }
+                  }
+        scriptPath("jenkins/Jenkinsfile")
+      }
+
+    }
+
+ }
